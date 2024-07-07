db00ea552c7ee8ee0ea10673e50352a61adb6ee2
Chris Jackson <chris.jackson@cjaxdevelopment.com>
2024-07-06 21:57:21 -0400

    Update axios baseURL for production

--- app/client/src/axiosConfig.js
+++ app/client/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000', // Fallback to localhost for development
 });
 
 instance.interceptors.request.use((config) => {
