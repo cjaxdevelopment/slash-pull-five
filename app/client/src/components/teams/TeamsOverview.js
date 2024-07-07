9705318e93207e27acc80287107bd48b8d9ae5cf
Chris Jackson <chris.jackson@cjaxdevelopment.com>
2024-07-07 08:41:53 -0400

    removed header and changed title

--- app/client/src/components/teams/TeamsOverview.js
+++ app/client/src/components/teams/TeamsOverview.js
@@ -75,7 +75,6 @@ const TeamsOverview = () => {
     <div>
       <Navbar onAddTeamClick={() => setIsAddTeamModalOpen(true)} showAddTeamButton={true} />
       <div className="container mx-auto p-4">
-        <h1 className="text-3xl font-bold text-center mb-4">Teams Overview</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {teams.map(team => (
             <TeamCard
