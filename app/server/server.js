f52d62275c573bd0d468504428390a12c00f37b6
Chris Jackson <chris.jackson@cjaxdevelopment.com>
2024-07-06 20:56:41 -0400

    procfile and steps to deploy

--- app/server/server.js
+++ app/server/server.js
@@ -27,13 +27,12 @@ app.use('/api/teams', teamRoutes);
 app.use('/api/players', playerRoutes);
 app.use('/api/user', userRoutes); 
 
-
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 }
 
