5b9ed39bf1de2a0f4f42c6e64ed1477c9843af45
Chris Jackson <chris.jackson@cjaxdevelopment.com>
2024-07-06 22:22:28 -0400

    logging for env variable

--- app/client/src/axiosConfig.js
+++ app/client/src/axiosConfig.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+console.log("REACT_APP_API_URL: ", process.env.REACT_APP_API_URL);
+
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000', // Fallback to localhost for development
 });
